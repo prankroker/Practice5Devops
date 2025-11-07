d8bd06aee44f44afe8f3fb3cba4fd12613a51df7
prankroker <prankroker2@gmail.com>
2025-11-07 19:37:16 +0200

    models tweaks

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -20,6 +24,10 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT,
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ['-publish']
         indexes = [
